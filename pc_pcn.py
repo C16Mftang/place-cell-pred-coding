5d17fcd01a3cb32907e8b6561e62e39464202fbe
Mufeng Tang <tangmufeng424@gmail.com>
2024-02-04 14:48:34 +0000

    adjusted visulization

--- pc_pcn.py
+++ pc_pcn.py
@@ -10,12 +10,35 @@ from model import *
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+def visualize_grid_cells(gcs, epoch):
+    # visualize latent space
+    n_show = 10
+    # set seed
+    np.random.seed(0)
+    select = np.random.choice(gcs.shape[1], n_show**2, replace=False)
+    # sort the select
+    select = np.sort(select)
+    gcs = gcs[:, select]
+    fig, axes = plt.subplots(n_show, n_show, figsize=(10,10))
+    for i, ax in enumerate(axes.flat):
+        gc = gcs[:,i].real.reshape((res, res))
+        gc = (gc - np.min(gc)) / (np.max(gc) - np.min(gc) + 1e-6)
+        im = ax.imshow(gc, cmap='jet')
+        ax.set_title(f'latent {select[i]}', fontsize=8)
+        ax.axis('off')
+
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+    fig.colorbar(im, cax=cbar_ax)
+    plt.savefig(options.save_dir + f'latent_space_epoch{epoch}.png')
+    plt.close()
+
 # Training options and hyperparameters
 class Options:
     pass
 options = Options()
 
-options.save_dir = './results/pcn/'
+options.save_dir = './models/pcn/'
 options.Np = 1000              # number of place cells
 options.Ng = 100             # number of grid cells
 options.place_cell_rf = 0.12  # width of place cell center tuning curve (m)
@@ -87,6 +110,14 @@ for i in range(options.learning_iters):
         print('Epoch', i)
         print('Loss', iter_loss)
 
+    # run inference every 50 epochs
+    if i == 0 or (i + 1) % (options.learning_iters // 10) == 0:
+        pcn.lamb = 0.
+        pcn.inference(X, options.inference_iters, inf_lr=1e-3)
+        gcs = pcn.val_nodes[0].clone().detach().cpu().numpy()
+        visualize_grid_cells(gcs, i)
+        pcn.lamb = options.lambda_hidden
+
     train_mse = iter_loss
     train_mses.append(train_mse)
     scheduler.step()
@@ -97,11 +128,7 @@ plt.xlabel('Epoch')
 plt.ylabel('Energy')
 plt.title('Training Loss')
 plt.savefig(options.save_dir + 'training_loss.png')
-
-# run inference on training data
-options.inference_lr = 1e-3
-pcn.lamb = 0.0
-pcn.inference(X, options.inference_iters, options.inference_lr)
+plt.close()
 
 # simply run PCA on the place cell activations
 pc_cov = np.cov(pc_outputs.cpu().numpy())
@@ -112,30 +139,8 @@ idx = eigvals.argsort()[::-1]
 eigvals = eigvals[idx]
 eigvecs = eigvecs[:,idx]
 
-# visualize latent space
-n_show = 10
-gcs = pcn.val_nodes[0].detach().cpu().numpy()
-# set seed
-np.random.seed(0)
-select = np.random.choice(gcs.shape[1], n_show**2, replace=False)
-# sort the select
-select = np.sort(select)
-gcs = gcs[:, select]
-print(gcs.shape)
-fig, axes = plt.subplots(n_show, n_show, figsize=(10,10))
-for i, ax in enumerate(axes.flat):
-    gc = gcs[:,i].real.reshape((res, res))
-    gc = (gc - np.min(gc)) / (np.max(gc) - np.min(gc) + 1e-6)
-    im = ax.imshow(gc, cmap='jet')
-    ax.set_title(f'latent {select[i]}', fontsize=8)
-    ax.axis('off')
-
-fig.subplots_adjust(right=0.8)
-cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
-fig.colorbar(im, cax=cbar_ax)
-plt.savefig(options.save_dir + 'latent_space.png')
-
 # visualize the eigenvecs
+n_show = 10
 fig, axes = plt.subplots(n_show, n_show, figsize=(10,10))
 for i, ax in enumerate(axes.flat):
     gc = eigvecs[:,i].real.reshape((res, res))
@@ -148,3 +153,4 @@ fig.subplots_adjust(right=0.8)
 cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
 fig.colorbar(im, cax=cbar_ax)
 plt.savefig(options.save_dir + 'eigenvectors.png')
+plt.close()
