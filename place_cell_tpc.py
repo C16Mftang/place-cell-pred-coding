98e08885540fe04c321cba2198d4828431198ceb
Mufeng <tangmufeng424@gmail.com>
2024-02-12 22:34:36 +0000

    tpc for path integration

--- place_cell_tpc.py
+++ place_cell_tpc.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import os
+import time
 
 from tqdm import tqdm
 from matplotlib import pyplot as plt
@@ -13,41 +14,14 @@ import predictive_coding as pc
 from place_cells import PlaceCells
 from trajectory_generator import TrajectoryGenerator
 from get_data import get_traj_loader, generate_traj_data
-from model import RNN
+from model import RNN, tPC, Bias
 from trainer import Trainer
 from utils import generate_run_ID
+from visualize import save_ratemaps
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 print(device)
-
-
-class tPCLayer(nn.Module):
-    def __init__(self, size_in, size_hidden):
-        super(tPCLayer, self).__init__()
-        # Initialize the Win and Wr linear layers according to the specified shapes
-        if size_in > 0:
-            self.Win = nn.Linear(size_in, size_hidden, bias=False)
-        self.Wr = nn.Linear(size_hidden, size_hidden, bias=False)
-        # determine whether there is an external input
-        self.is_in = True if size_in > 0 else False
-
-    def forward(self, inputs):
-        # input: a tuple of two tensors (input, hidden (from the previous time step))
-        # Compute Win(input) + Wr(hidden) and return the result
-        return self.Win(inputs[1]) + self.Wr(inputs[0]) if self.is_in else self.Wr(inputs[0])
-
-class InitEncoder(nn.Module):
-    def __init__(self, size_v, size_p, size_hidden):
-        super(InitEncoder, self).__init__()
-        # Initialize the Win and Wr linear layers according to the specified shapes
-        self.Wv = nn.Linear(size_v, size_hidden, bias=False)
-        self.Wp = nn.Linear(size_p, size_hidden, bias=False)
-
-    def forward(self, inputs):
-        # input: a tuple of two tensors (velocity, init_pos)
-        # Compute Win(input) + Wr(hidden) and return the result
-        return self.Wv(inputs[0]) + self.Wp(inputs[1])
 
 def mse_loss(output, _target):
     return (output - _target).pow(2).sum() * 0.5
@@ -62,14 +36,14 @@ class Options:
     pass
 options = Options()
 
-options.n_epochs = 500          # number of training epochs
-options.n_steps = 20          # number of batches in one epoch
-options.batch_size = 100        # number of trajectories per batch
+options.n_epochs = 10          # number of training epochs
+options.n_steps = 2          # number of batches in one epoch
+options.batch_size = 10        # number of trajectories per batch
 options.sequence_length = 20    # number of steps in trajectory
-options.learning_rate = 1e-4    # gradient descent learning rate
+options.learning_rate = 1e-3    # gradient descent learning rate
 options.size_in = 2             # dimension of velocity input
 options.Np = 512                # number of place cells
-options.Ng = 2048               # number of grid cells
+options.Ng = 256               # number of grid cells
 options.place_cell_rf = 0.12    # width of place cell center tuning curve (m)
 options.surround_scale = 2      # if DoG, ratio of sigma2^2 to sigma1^2
 options.RNN_type = 'tPC'        # RNN or LSTM
@@ -85,58 +59,30 @@ options.data_source = 'online'
 options.run_ID = generate_run_ID(options)
 options.decay_step_size = 5
 options.decay_rate = 1
-activations = {
-    'relu': nn.ReLU(),
-    'tanh': nn.Tanh(),
-}
+options.save_every = 2
+
+ckpt_dir = os.path.join(options.save_dir, options.run_ID)
+if not os.path.isdir(ckpt_dir):
+    os.makedirs(ckpt_dir, exist_ok=True)
+print("Saving to: {}".format(ckpt_dir))
 
 # define place cells, trajectory generator
 place_cells = PlaceCells(options)
 trajectory_generator = TrajectoryGenerator(options, place_cells)
-
-init_layer = nn.Sequential(
-    InitEncoder(options.size_in, options.Np, options.Ng),
-    pc.PCLayer(),
-    activations[options.activation],
-)
-
-rec_layer = nn.Sequential(
-    tPCLayer(options.size_in, options.Ng),
-    pc.PCLayer(),
-    activations[options.activation],
-)
-
-forward_layer = nn.Sequential(
-    nn.Linear(options.Ng, options.Np),
-    pc.PCLayer(),
-    # activations[options.activation],
-)
-
-# to get the initial grid cell
-encoder = nn.Sequential(
-    init_layer,
-    forward_layer,
-).to(device)
-
-model = nn.Sequential(
-    rec_layer,
-    forward_layer,
-).to(device)
-
+model = tPC(options).to(device)
 model.train()
-encoder.train()
 
-enc_trainer = pc.PCTrainer(
-    encoder, 
-    T=200,
-    update_x_at='all',
-    optimizer_x_fn=optim.SGD,
-    optimizer_x_kwargs={"lr": 1e-2},
-    update_p_at='last',
-    optimizer_p_fn=optim.Adam,
-    optimizer_p_kwargs={"lr": options.learning_rate},
-    plot_progress_at=[],
-)
+# init_trainer = pc.PCTrainer(
+#     model.init_model,
+#     T=20,
+#     update_x_at='all',
+#     optimizer_x_fn=optim.SGD,
+#     optimizer_x_kwargs={"lr": 1e-2},
+#     update_p_at='last',
+#     optimizer_p_fn=optim.Adam,
+#     optimizer_p_kwargs={"lr": options.learning_rate},
+#     plot_progress_at=[],
+# )
 
 tpc_trainer = pc.PCTrainer(
     model, 
@@ -154,32 +100,44 @@ gen = trajectory_generator.get_generator()
 
 losses = []
 for i in range(options.n_epochs):
+    print(f'Epoch {i+1}/{options.n_epochs}')
+    start = time.time()
     epoch_loss = 0
+    epoch_err = 0
     for step_idx in range(options.n_steps):
         inputs, pc_outputs, pos = next(gen)
         v, p0 = inputs
+
         assert options.sequence_length == v.shape[0], "sequence length not match"
+
+        hidden = p0 @ trajectory_generator.get_hidden_projector()
+        init_state = hidden.clone()
         for k in range(options.sequence_length):
-            if k == 0:
-                results = enc_trainer.train_on_batch(
-                    inputs=(v[k], p0),
-                    loss_fn=ce_loss,
-                    loss_fn_kwargs={"_target": pc_outputs[k]},
-                    is_log_progress=False,
-                    is_return_results_every_t=False,
-                )
-                hidden = encoder[0][1].get_x().detach().clone()
-            else:
-                result = tpc_trainer.train_on_batch(
-                    inputs=(hidden, v[k]),
-                    loss_fn=ce_loss,
-                    loss_fn_kwargs={"_target": pc_outputs[k]}, # can add weight decay here
-                    is_log_progress=False,
-                    is_return_results_every_t=False,
-                )
-                hidden = model[0][1].get_x().detach().clone()
+            results = tpc_trainer.train_on_batch(
+                inputs=(hidden, v[k]),
+                loss_fn=ce_loss,
+                loss_fn_kwargs={"_target": pc_outputs[k]}, # can add weight decay here
+                is_log_progress=False,
+                is_return_results_every_t=False,
+            )
+            hidden = model.tpc[0][1].get_x().detach()
             # log the output loss
             epoch_loss += results["loss"][0]
+
+        # log the decoded position error
+        pred = model.predict(v, init_state)
+        pred_pos = place_cells.get_nearest_cell_pos(pred)
+        epoch_err += torch.sqrt(((pos - pred_pos)**2).sum(-1)).mean()
+    
     epoch_loss /= (options.sequence_length * options.n_steps)
+    epoch_err /= (options.n_steps)
     losses.append(epoch_loss)
-    print(f'epoch{i}, loss: {epoch_loss}')
+    print(f'loss: {epoch_loss}, decoded position error: {epoch_err} m, time: {time.time() - start}')
+
+    if (i + 1) % options.save_every == 0:
+        torch.save(
+            model.state_dict(), 
+            os.path.join(ckpt_dir, f'epoch_{i+1}.pth')
+        )
+        save_ratemaps(model, trajectory_generator, options, i+1, res=20, n_avg=1000, pc=True)
+# predict after training
