e93ce9a6e84e4a34d1bff55481a6ba2c8fd00c87
Mufeng <tangmufeng424@gmail.com>
2024-03-20 20:44:57 +0000

    unify tpc and rnn training

--- place_cell_rnn.py
+++ place_cell_rnn.py
@@ -2,78 +2,61 @@ import numpy as np
 import torch
 import os
 import time
-
-from tqdm import tqdm
 from matplotlib import pyplot as plt
 
-# from utils import generate_run_ID, load_trained_weights
 from place_cells import PlaceCells
 from trajectory_generator import TrajectoryGenerator
-from get_data import get_traj_loader, generate_traj_data
 from model import RNN
 from trainer import Trainer
-from utils import generate_run_ID
+from visualize import *
 
 # Training hyperparameters to fully reproduce Sorscher et al. 2023
 class Options:
-    full_size = 50000
-    n_epochs = 500          # number of training epochs
-    n_steps = 2000          # number of batches in one epoch
-    batch_size = 100        # number of trajectories per batch
-    sequence_length = 20    # number of steps in trajectory
-    learning_rate = 1e-4    # gradient descent learning rate
-    Np = 200                # number of place cells
-    Ng = 50               # number of grid cells
-    place_cell_rf = 0.12    # width of place cell center tuning curve (m)
-    surround_scale = 2      # if DoG, ratio of sigma2^2 to sigma1^2
-    RNN_type = 'RNN'        # RNN or LSTM
-    activation = 'relu'     # recurrent nonlinearity
-    weight_decay = 1e-4     # strength of weight decay on recurrent weights
-    DoG = True              # use difference of gaussians tuning curves
-    periodic = False        # trajectories with periodic boundary conditions
-    box_width = 1.6         # width of training environment
-    box_height = 1.6        # height of training environment
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    data_source = 'online'
-    decay_step_size = 5
-    decay_rate = 1
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    oned = False
+    Np = 100
+    Ng = 20
+    Nv = 2
+    DoG = False
+    box_width = 1.6         
+    box_height = 1.6 
+    place_cell_rf = 0.12 
+    surround_scale = 2
+    periodic = False
+    sequence_length = 20
+    dt = 0.02
+    batch_size = 500
+    n_epochs = 10
+    n_steps = 10
+    learning_rate = 1e-4    
+    weight_decay = 1e-4    
+    decay_step_size = 50
+    decay_rate = 0.9
+    activation = 'relu'   
+    restore = None # if restore, this should be the timestamp of the saved model
+    preloaded_data = False
+    save = False 
+    save_every = 100
 options = Options()
 
 # save directory
 now = time.strftime('%b-%d-%Y-%H-%M-%S', time.gmtime(time.time()))
+if options.restore is not None:
+    now = options.restore
 options.save_dir = os.path.join('./results/rnn', now)
 
 if not os.path.exists(options.save_dir):
     os.makedirs(options.save_dir)
 print('Saving to:', options.save_dir)
 
-
 # define place cells, trajectory generator, model, and trainer
-place_cells = PlaceCells(options)
-model = RNN(options, place_cells).to(options.device)
-trajectory_generator = TrajectoryGenerator(options, place_cells)
-trainer = Trainer(options, model, trajectory_generator, restore=False)
+place_cell = PlaceCells(options)
+generator = TrajectoryGenerator(options, place_cell)
+model = RNN(options, place_cell).to(options.device)
+trainer = Trainer(options, model, generator, place_cell, restore=options.restore)
 
-if options.data_source == 'pre':
-    path = f'data/trajectory_{options.full_size}_{options.sequence_length}_{options.Np}.npz'
-    # check if the file exists
-    if os.path.exists(path):
-        print('Loading pre-generated data')
-    else:
-        print('Generating new data')
-        generate_traj_data(options)
-
-    dataloader = get_traj_loader(path, options)
-    trainer.train_batch(dataloader, n_epochs=options.n_epochs, save=True)
-else:
-    trainer.train(n_epochs=options.n_epochs, n_steps=options.n_steps, save=True)
-
-plt.figure(figsize=(12,3))
-plt.subplot(121)
-plt.plot(trainer.err, c='black')
-
-plt.title('Decoding error (m)'); plt.xlabel('train step')
-plt.subplot(122)
-plt.plot(trainer.loss, c='black');
-plt.title('Loss'); plt.xlabel('train step');
-plt.savefig(os.path.join(options.save_dir, 'loss'))
+trainer.train(preloaded_data=options.preloaded_data, save=options.save)
+plot_2d_performance(place_cell, generator, options, trainer)
+rate_map = compute_ratemaps(model, trainer, generator, options, res=20, n_avg=200, Ng=options.Ng)
+plot_2d_ratemaps(rate_map, options, n_col=4)
+plot_loss_err(trainer, options)
--- place_cell_tpc.py
+++ place_cell_tpc.py
@@ -1,66 +1,68 @@
 import numpy as np
 import torch
-import scipy
-import matplotlib.pyplot as plt
 import os
-import pickle
 import time
+import matplotlib.pyplot as plt
 
 from place_cells import PlaceCells
 from trajectory_generator import TrajectoryGenerator
-
-from model import *
+from model import HierarchicalPCN, TemporalPCN
 from trainer import PCTrainer
 from visualize import *
-import utils
 
-class Options():
+class Options:
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     oned = False
     Np = 100
     Ng = 20
     Nv = 2
     DoG = False
-    box_width = 1.6         # width of training environment
+    box_width = 1.6         
     box_height = 1.6 
-    place_cell_rf = 0.12 # None for one-hot encoding of place cells
+    place_cell_rf = 0.12
     surround_scale = 2
-    plce_cell_seed = 0
     periodic = False
     sequence_length = 20
-    dt = 0.01
+    dt = 0.02
     batch_size = 500
     n_epochs = 10
     n_steps = 10
     learning_rate = 2e-3
     weight_decay = 1e-3
-    decay_step_size = 10
+    decay_step_size = 50
     decay_rate = 0.9 
-    lambda_z = 0. # sparse penalty for the temporal model
-    lambda_z_init = 0. # sparse penalty for the initial static model
+    lambda_z = 0. 
+    lambda_z_init = 0. 
     inf_iters = 20 
-    test_inf_iters = 300 # inference iterations during testing, for the initial static model
+    test_inf_iters = 300 
     inf_lr = 1e-2
     out_activation = utils.Softmax()
     rec_activation = utils.Tanh()
+    restore = None # if restore, this should be the timestamp of the saved model
+    preloaded_data = True
+    save = False
+    save_every = 100
 options = Options()
 
 # save directory
 now = time.strftime('%b-%d-%Y-%H-%M-%S', time.gmtime(time.time()))
+if options.restore is not None:
+    now = options.restore
 options.save_dir = os.path.join('./results/tpc', now)
 
 if not os.path.exists(options.save_dir):
     os.makedirs(options.save_dir)
 print('Saving to:', options.save_dir)
 
-# define place cells, trajectory generator
+# define place cells, trajectory generator, model, and trainer
 place_cell = PlaceCells(options)
 generator = TrajectoryGenerator(options, place_cell)
 model = TemporalPCN(options).to(options.device)
 init_model = HierarchicalPCN(options).to(options.device)
-trainer = PCTrainer(options, model, init_model, generator, place_cell, restore=False)
+trainer = PCTrainer(options, model, init_model, generator, place_cell, restore=options.restore)
 
-trainer.train(save=False, preloaded_data=False)
+trainer.train(preloaded_data=options.preloaded_data, save=options.save)
 plot_2d_performance(place_cell, generator, options, trainer)
 rate_map = compute_ratemaps(model, trainer, generator, options, res=20, n_avg=200, Ng=options.Ng)
 plot_2d_ratemaps(rate_map, options, n_col=4)
+plot_loss_err(trainer, options)
