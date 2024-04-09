33e5b372dae4d51bda4a753c8c94f1a9f44bb8bb
Mufeng Tang <tangmufeng424@gmail.com>
2024-04-09 11:41:38 +0100

    change hps

--- place_cell_tpc.py
+++ place_cell_tpc.py
@@ -14,7 +14,7 @@ class Options:
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     oned = False
     Np = 512
-    Ng = 256
+    Ng = 4096
     Nv = 2
     DoG = True
     box_width = 1.6         
@@ -25,16 +25,16 @@ class Options:
     sequence_length = 20
     dt = 0.02
     batch_size = 500
-    n_epochs = 100
+    n_epochs = 400
     n_steps = 100
     learning_rate = 1e-4
     weight_decay = 0
     decay_step_size = 50
     decay_rate = 0.9 
-    lambda_z = 0.2
-    lambda_z_init = 0.2
+    lambda_z = 0.
+    lambda_z_init = 0.1
     inf_iters = 20 
-    test_inf_iters = 300 
+    test_inf_iters = 20
     inf_lr = 1e-2
     out_activation = utils.Tanh()
     rec_activation = utils.Tanh()
@@ -54,6 +54,8 @@ if not os.path.exists(options.save_dir):
     os.makedirs(options.save_dir)
 print('Saving to:', options.save_dir)
 
+utils.save_options_to_json(options, os.path.join(options.save_dir, 'configs.json'))
+
 # define place cells, trajectory generator, model, and trainer
 place_cell = PlaceCells(options)
 generator = TrajectoryGenerator(options, place_cell)
@@ -67,4 +69,3 @@ plot_2d_performance(place_cell, generator, options, trainer)
 rate_map = compute_ratemaps(model, trainer, generator, options, res=20, n_avg=200, Ng=options.Ng)
 plot_2d_ratemaps(rate_map, options, n_col=4)
 plot_loss_err(trainer, options)
-utils.save_options_to_json(options, os.path.join(options.save_dir, 'configs.json'))
