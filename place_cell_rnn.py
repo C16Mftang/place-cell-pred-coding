7acbf4ced27852fbf4c0095e4fad28ca55a8ba00
Mufeng <tangmufeng424@gmail.com>
2024-04-11 12:15:47 +0100

    add argparser

--- place_cell_rnn.py
+++ place_cell_rnn.py
@@ -2,43 +2,49 @@ import numpy as np
 import torch
 import os
 import time
-from matplotlib import pyplot as plt
+import argparse
 
 from place_cells import PlaceCells
 from trajectory_generator import TrajectoryGenerator
 from model import RNN
 from trainer import Trainer
 from visualize import *
+import argparse
 
 # Training hyperparameters to fully reproduce Sorscher et al. 2023
-class Options:
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-    oned = False
-    Np = 512
-    Ng = 4096
-    Nv = 2
-    DoG = True
-    box_width = 1.6         
-    box_height = 1.6 
-    place_cell_rf = 0.12 
-    surround_scale = 2
-    periodic = True
-    sequence_length = 20
-    dt = 0.02
-    batch_size = 500
-    n_epochs = 200
-    n_steps = 100
-    learning_rate = 1e-4    
-    weight_decay = 1e-4    
-    decay_step_size = 50
-    decay_rate = 0.9
-    activation = 'relu'   
-    restore = None # if restore, this should be the timestamp of the saved model
-    preloaded_data = False
-    save = False 
-    save_every = 100
-    loss = 'MSE'
-options = Options()
+parser = argparse.ArgumentParser(description='Place Cell RNN Training')
+
+# Training hyperparameters to fully reproduce Sorscher et al. 2023
+parser.add_argument('--device', type=str, default='cuda:0' if torch.cuda.is_available() else 'cpu', help='Device to use for training')
+parser.add_argument('--oned', type=bool, default=False, help='Whether to use one-dimensional place cells')
+parser.add_argument('--Np', type=int, default=512, help='Number of place cells')
+parser.add_argument('--Ng', type=int, default=2048, help='Number of grid cells')
+parser.add_argument('--Nv', type=int, default=2, help='Number of velocity inputs')
+parser.add_argument('--DoG', type=bool, default=True, help='Whether to use Difference of Gaussians for place cell RFs')
+parser.add_argument('--box_width', type=float, default=1.6, help='Width of the environment box')
+parser.add_argument('--box_height', type=float, default=1.6, help='Height of the environment box')
+parser.add_argument('--place_cell_rf', type=float, default=0.12, help='Place cell receptive field size')
+parser.add_argument('--surround_scale', type=int, default=2, help='Scale factor for the surround inhibition')
+parser.add_argument('--periodic', type=bool, default=True, help='Whether the environment is periodic')
+parser.add_argument('--sequence_length', type=int, default=20, help='Length of the trajectory sequence')
+parser.add_argument('--dt', type=float, default=0.02, help='Time step size')
+parser.add_argument('--batch_size', type=int, default=500, help='Batch size for training')
+parser.add_argument('--n_epochs', type=int, default=200, help='Number of training epochs')
+parser.add_argument('--n_steps', type=int, default=100, help='Number of steps per epoch')
+parser.add_argument('--learning_rate', type=float, default=1e-4, help='Learning rate for training')
+parser.add_argument('--weight_decay', type=float, default=1e-4, help='Weight decay for training')
+parser.add_argument('--decay_step_size', type=int, default=50, help='Step size for learning rate decay')
+parser.add_argument('--decay_rate', type=float, default=0.9, help='Decay rate for learning rate decay')
+parser.add_argument('--rec_activation', type=str, default='relu', help='Recurrent activation function for the RNN')
+parser.add_argument('--out_activation', type=str, default='softmax', help='Output activation function for the RNN')
+parser.add_argument('--restore', type=str, default=None, help='Timestamp of the saved model to restore')
+parser.add_argument('--preloaded_data', type=bool, default=False, help='Whether to use preloaded data')
+parser.add_argument('--save', type=bool, default=False, help='Whether to save the model')
+parser.add_argument('--save_every', type=int, default=100, help='Save the model every n epochs')
+parser.add_argument('--loss', type=str, default='MSE', help='Loss function for training')
+parser.add_argument('--is_wandb', type=bool, default=False, help='Whether to use wandb for logging')
+
+options = parser.parse_args()
 
 # save directory
 now = time.strftime('%b-%d-%Y-%H-%M-%S', time.gmtime(time.time()))
@@ -49,6 +55,8 @@ options.save_dir = os.path.join('./results/rnn', now)
 if not os.path.exists(options.save_dir):
     os.makedirs(options.save_dir)
 print('Saving to:', options.save_dir)
+
+utils.save_options_to_json(options, os.path.join(options.save_dir, 'configs.json'))
 
 # define place cells, trajectory generator, model, and trainer
 place_cell = PlaceCells(options)
@@ -62,4 +70,3 @@ plot_2d_performance(place_cell, generator, options, trainer)
 rate_map = compute_ratemaps(model, trainer, generator, options, res=20, n_avg=200, Ng=options.Ng)
 plot_2d_ratemaps(rate_map, options, n_col=4)
 plot_loss_err(trainer, options)
-utils.save_options_to_json(options, os.path.join(options.save_dir, 'configs.json'))
