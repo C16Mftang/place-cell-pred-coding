2b5845bdfaf46fcba4d411939025ed9382bc24ef
Mufeng Tang <tangmufeng424@gmail.com>
2025-04-28 11:38:21 +0100

    integrate pcn training code with others

--- place_cell_pcn.py
+++ place_cell_pcn.py
@@ -5,43 +5,16 @@ from matplotlib import pyplot as plt
 import argparse
 import time
 import json
+import wandb
 from tqdm import tqdm
 
 from src.data.place_cells import PlaceCells
 from src.data.trajectory_generator import TrajectoryGenerator
 
 from src.model import *
-from src.visualize import compute_grid_scores
+from src.visualize import compute_grid_scores, plot_all_ratemaps
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-
-
-def plot_all_ratemaps(rate_map, scores, options):
-    n_col = 8
-    Ng_per_file = 64
-    n_files = options.Ng // Ng_per_file
-
-    all_dir = os.path.join(options.save_dir, "all_maps")
-    if not os.path.exists(all_dir):
-        os.makedirs(all_dir)
-
-    for i in tqdm(range(n_files)):
-        rm = rate_map[
-            i * Ng_per_file : (i + 1) * Ng_per_file
-        ]  # [Ng_per_file, res, res]
-        fig, axes = plt.subplots(
-            Ng_per_file // n_col, n_col, figsize=(n_col, Ng_per_file // n_col)
-        )
-        for j, ax in enumerate(axes.flatten()):
-            r = (rm[j] - rm[j].min()) / (rm[j].max() - rm[j].min() + 1e-9)
-            ax.imshow(r, cmap="jet")
-            ax.set_xticks([])
-            ax.set_yticks([])
-            ax.set_title(f"{scores[j+i*Ng_per_file]:.2f}")
-        plt.tight_layout()
-        plt.savefig(os.path.join(all_dir, f"2d_ratemaps_{i}.png"))
-        plt.close(fig)
-
 
 # Training options and hyperparameters
 parser = argparse.ArgumentParser()
@@ -86,31 +59,37 @@ parser.add_argument(
     "--box_height", type=float, default=1.6, help="Height of training environment"
 )
 parser.add_argument(
+    "--env_shape",
+    type=str,
+    default='rectangle',
+    help="Shape of the simulated environment."
+)
+parser.add_argument(
     "--out_activation", type=str, default="linear", help="Nonlinearity function"
 )
 parser.add_argument(
-    "--inference_lr", type=float, default=1e-2, help="Learning rate for inference"
+    "--inf_lr", type=float, default=1e-2, help="Learning rate for inference"
 )
 parser.add_argument(
-    "--inference_lr_test",
+    "--inf_lr_test",
     type=float,
     default=1e-3,
     help="Learning rate for inference during testing",
 )
 parser.add_argument(
-    "--inference_iters", type=int, default=20, help="Number of inference iterations"
+    "--inf_iters", type=int, default=10, help="Number of inference iterations"
 )
 parser.add_argument(
-    "--inference_iters_test",
+    "--inf_iters_test",
     type=int,
     default=10,
     help="Number of inference iterations during test",
 )
 parser.add_argument(
-    "--learning_lr", type=float, default=1e-4, help="Learning rate for training"
+    "--learning_rate", type=float, default=1e-4, help="Learning rate for training"
 )
 parser.add_argument(
-    "--learning_iters", type=int, default=400, help="Number of training iterations"
+    "--n_epochs", type=int, default=100, help="Number of training iterations"
 )
 parser.add_argument("--batch_size", type=int, default=100, help="Batch size")
 parser.add_argument("--res", type=int, default=30, help="Sample resolution")
@@ -141,7 +120,7 @@ parser.add_argument(
     "--rf_std", type=float, default=0, help="Standard deviation of place cell RFs"
 )
 parser.add_argument("--save_every", type=int, default=2, help="Save model interval")
-parser.add_argument("--sample_size", type=int, default=100000)
+parser.add_argument("--sample_size", type=int, default=1000)
 parser.add_argument(
     "--place_cell_rf_prob", 
     type=float, 
@@ -155,24 +134,34 @@ parser.add_argument(
     default="train",
     help="Mode for running the model; input run folder name for model inspection",
 )
+parser.add_argument(
+    "--is_wandb",
+    type=lambda x: (str(x).lower() == "true"),
+    default=False,
+    help="Whether to use wandb for logging",
+)
+parser.add_argument(
+    "--sweep",
+    type=lambda x: (str(x).lower() == "true"),
+    default=False,
+    help="Hyperparameter tune",
+)
 options = parser.parse_args()
 
 if options.mode == 'train':
-    now = time.strftime("%b-%d-%Y-%H-%M-%S", time.gmtime(time.time()))
-    options.save_dir = os.path.join("./results/pcn", now)
-    if not os.path.exists(options.save_dir):
-        os.makedirs(options.save_dir)
-    if not os.path.exists(os.path.join(options.save_dir, "models")):
-        os.makedirs(os.path.join(options.save_dir, "models"))
-    print("Saving to:", options.save_dir)
-    utils.save_options_to_json(options, os.path.join(options.save_dir, "configs.json"))
+    if not options.sweep:
+        now = time.strftime("%b-%d-%Y-%H-%M-%S", time.gmtime(time.time()))
+        options.save_dir = os.path.join("./results/pcn", now)
+        if not os.path.exists(options.save_dir):
+            os.makedirs(options.save_dir)
+        if not os.path.exists(os.path.join(options.save_dir, "models")):
+            os.makedirs(os.path.join(options.save_dir, "models"))
+        print("Saving to:", options.save_dir)
+        utils.save_options_to_json(options, os.path.join(options.save_dir, "configs.json"))
 
     place_cells = PlaceCells(options)
     trajectory_generator = TrajectoryGenerator(options, place_cells)
 
-    # Generate 500k random positions that covers the space
-    # each position can then be represented as Np-dimensional vector by place cells
-    # this provides a 30x30 approximation of the whole environment
     usx = np.random.uniform(
         -options.box_width / 2, options.box_width / 2, (options.sample_size,)
     )
@@ -199,20 +188,23 @@ if options.mode == 'train':
     ).to(device)
     X = pc_outputs.to(device)
     optimizer = torch.optim.Adam(
-        pcn.parameters(), lr=options.learning_lr, weight_decay=options.weight_decay
+        pcn.parameters(), lr=options.learning_rate, weight_decay=options.weight_decay
     )
     scheduler = torch.optim.lr_scheduler.StepLR(
         optimizer, step_size=options.decay_step_size, gamma=options.decay_rate
     )
 
     train_mses = []
-    for i in range(options.learning_iters):
+    norms = []
+    if options.is_wandb == True and options.sweep == False:
+        wandb.init(project='place-cell-pcn', config=options)
+    for epoch in range(options.n_epochs):
         iter_loss = 0
         tbar = tqdm(range(0, options.sample_size, options.batch_size))
         for batch_idx in tbar:
             data = X[batch_idx : batch_idx + options.batch_size]
             optimizer.zero_grad()
-            pcn.inference(data, options.inference_iters, options.inference_lr)
+            pcn.inference(data, options.inf_iters, options.inf_lr)
             loss = pcn.get_energy()
             loss.backward()
             optimizer.step()
@@ -220,29 +212,36 @@ if options.mode == 'train':
 
             tbar.set_description(
                 "Epoch: {}/{}. Loss: {}".format(
-                    i + 1, options.learning_iters, np.round(loss.item(), 8),
+                    epoch + 1, options.n_epochs, np.round(loss.item(), 8),
                 )
             )
 
         train_mse = iter_loss / (options.sample_size / options.batch_size)
-        print(train_mse)
+        if options.is_wandb:
+            wandb.log({
+                'energy': train_mse,
+            })
         train_mses.append(train_mse)
         scheduler.step()
 
-        if (i + 1) % options.save_every == 0:
+        if (epoch + 1) % options.save_every == 0 and not options.sweep:
             torch.save(
                 pcn.state_dict(),
-                os.path.join(options.save_dir, "models", f"model{i+1}.pth"),
+                os.path.join(options.save_dir, "models", f"model{epoch + 1}.pth"),
             )
+    
+    if not options.sweep:
+        # Plot the training loss and save
+        np.save(os.path.join(options.save_dir, "training_loss.npy"), np.array(train_mses))
+        plt.plot(train_mses)
+        plt.xlabel("Epoch")
+        plt.ylabel("Energy")
+        plt.title("Training Loss")
+        plt.savefig(os.path.join(options.save_dir, "training_loss.png"))
+        plt.close()
 
-    # Plot the training loss and save
-    np.save(os.path.join(options.save_dir, "training_loss.npy"), np.array(train_mses))
-    plt.plot(train_mses)
-    plt.xlabel("Epoch")
-    plt.ylabel("Energy")
-    plt.title("Training Loss")
-    plt.savefig(os.path.join(options.save_dir, "training_loss.png"))
-    plt.close()
+    if options.is_wandb:
+        wandb.finish()
 
 else:
     now = options.mode
@@ -271,7 +270,7 @@ else:
         relu_inf=options.relu_inf,
     ).to(device)
 
-    ckpt = torch.load(os.path.join(save_dir, "models", f"model{options.learning_iters}.pth"))
+    ckpt = torch.load(os.path.join(save_dir, "models", f"model{options.n_epochs}.pth"))
     options.save_dir = save_dir
     pcn.load_state_dict(ckpt)
 
@@ -291,7 +290,7 @@ else:
         pc_outputs_test = pc_outputs_test.reshape((-1, options.Np))
         pc_outputs_test = pc_outputs_test - pc_outputs_test.mean(dim=0, keepdim=True)
         pcn.inference(
-            pc_outputs_test, options.inference_iters_test, options.inference_lr_test
+            pc_outputs_test, options.inf_iters_test, options.inf_lr_test
         )
         g_batch = pcn.val_nodes[0].clone().detach().cpu().numpy()  # [Ng, res**2]
         pos_batch = np.reshape(pos_batch.cpu().detach().numpy(), [-1, 2])
@@ -324,5 +323,6 @@ else:
 
     idx, scores, sacs = compute_grid_scores(options.res, activations, options)
     sorted_gcs = activations[idx]
-    plot_all_ratemaps(sorted_gcs, scores, options)
+    plot_all_ratemaps(sorted_gcs, options, scores=scores)
+    np.save(os.path.join(save_dir, "grid_scores.npy"), scores)
 
